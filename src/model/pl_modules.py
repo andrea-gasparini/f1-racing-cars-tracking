99d7a36de221dc6131f1ba0b65697de34178191a
Andrea Gasparini <andrea.gasparini11@gmail.com>
2021-12-01 18:40:14 +0100

    VGG16 transfer learning implementation draft
    Co-authored-by: Edoardo Di Paolo <aedoardo@users.noreply.github.com>

--- src/model/pl_modules.py
+++ src/model/pl_modules.py
@@ -1,15 +1,50 @@
-from typing import Any
-
 import pytorch_lightning as pl
+import torchvision.models as models
 import torch
 
+from typing import Any, List
+from torch import Tensor
+from torch.nn.modules.dropout import Dropout
+from torch.nn import Module, Sequential, Linear, Dropout, ReLU, Sigmoid, MSELoss
 
-class RacingF1Module(pl.LightningModule):
-    def __init__(self, conf, *args, **kwargs) -> None:
+from src.utils import paths_images_to_tensor
+
+
+class RacingF1Detector(pl.LightningModule):
+
+    def __init__(self, hparams = {}, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.save_hyperparameters(conf)
 
-    def forward(self, **kwargs) -> dict:
+        self.save_hyperparameters(hparams)
+        self.loss_function = MSELoss()
+
+        vgg16 = models.vgg16(pretrained=True)
+        vgg16.training = False
+        
+        for param in vgg16.features.parameters():
+            param.requires_grad = False
+            
+        vgg_layers: List[Module] = list(vgg16.classifier.children())
+        vgg_last_linear_layer: Linear = vgg_layers[-1]
+        vgg_out_size: int = vgg_last_linear_layer.out_features
+
+        head_layers = Sequential(
+            *vgg_layers,
+            Dropout(0.5),
+            Linear(vgg_out_size, 128),
+            ReLU(),
+            Linear(128, 64),
+            ReLU(),
+            Linear(64, 32),
+            ReLU(),
+            Linear(32, 4),
+            Sigmoid()
+        )
+
+        self.model = torch.nn.Sequential(head_layers)
+
+
+    def forward(self, x: List[Tensor], **kwargs) -> dict:
         """
         Method for the forward pass.
         'training_step', 'validation_step' and 'test_step' should call
@@ -19,46 +54,44 @@ class RacingF1Module(pl.LightningModule):
             output_dict: forward output containing the predictions (output logits ecc...) and the loss if any.
 
         """
-        output_dict = {}
-        return output_dict
+        out = self.model(x)
+        return out
 
-    def training_step(self, batch: dict, batch_idx: int) -> torch.Tensor:
-        forward_output = self.forward(**batch)
-        self.log("loss", forward_output["loss"])
-        return forward_output["loss"]
+
+    def training_step(self, batch: dict, batch_idx: int) -> Tensor:
+
+        inputs = paths_images_to_tensor(batch['img_path'])
+        labels = batch['bounding_box']
+
+        logits = self.forward(inputs)
+
+        loss = self.loss_function(logits[0], labels['x'])
+        loss += self.loss_function(logits[1], labels['y'])
+        loss += self.loss_function(logits[2], labels['width'])
+        loss += self.loss_function(logits[3], labels['height'])
+
+        return loss
+
 
     def validation_step(self, batch: dict, batch_idx: int) -> None:
-        forward_output = self.forward(**batch)
-        self.log("val_loss", forward_output["loss"])
+        raise NotImplementedError
+
 
     def test_step(self, batch: dict, batch_idx: int) -> Any:
         raise NotImplementedError
 
+
     def training_epoch_end(self, outputs):
         raise NotImplementedError
+
 
     def validation_epoch_end(self, outputs):
         raise NotImplementedError
 
+
     def test_epoch_end(self, outputs):
         raise NotImplementedError
 
+
     def configure_optimizers(self):
-        """
-        FROM PYTORCH LIGHTNING DOCUMENTATION
-
-        Choose what optimizers and learning-rate schedulers to use in your optimization.
-        Normally you'd need one. But in the case of GANs or similar you might have multiple.
-
-        Return:
-            Any of these 6 options.
-
-            - Single optimizer.
-            - List or Tuple - List of optimizers.
-            - Two lists - The first list has multiple optimizers, the second a list of LR schedulers (or lr_dict).
-            - Dictionary, with an 'optimizer' key, and (optionally) a 'lr_scheduler'
-              key whose value is a single LR scheduler or lr_dict.
-            - Tuple of dictionaries as described, with an optional 'frequency' key.
-            - None - Fit will run without any optimizer.
-        """
-        raise NotImplementedError
+        return torch.optim.Adam(self.parameters())
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,16 @@
 from typing import List
+from torch import Tensor
+from PIL import Image
+from torchvision import transforms
 
 import os
 
 def join_dirs(base_dir: str, subdirs: List[str]) -> List[str]:
     return [os.path.join(base_dir, subdir) for subdir in subdirs]
+
+def paths_images_to_tensor(images_paths: List[str]) -> List[Tensor]:
+    images = []
+    for image_path in images_paths:
+        images.append(transforms.ToTensor(Image.open(image_path).convert('RGB')))
+    return images
+        
