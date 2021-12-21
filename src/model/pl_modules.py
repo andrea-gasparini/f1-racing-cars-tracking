850d9b4f063a056eb89425c86994c9b5d4b507bd
Andrea Gasparini <andrea.gasparini11@gmail.com>
2021-12-21 12:54:18 +0100

    Add generic model step and metrics tracking in val and test steps

--- src/model/pl_modules.py
+++ src/model/pl_modules.py
@@ -1,14 +1,9 @@
 import pytorch_lightning as pl
 import torch
+import torchmetrics
 
-from typing import Any
+from typing import Dict, List, Union
 from torch import Tensor
-from torch.nn.modules.activation import ReLU, Sigmoid
-from torch.nn.modules.container import Sequential
-from torch.nn.modules.conv import Conv2d
-from torch.nn.modules.flatten import Flatten
-from torch.nn.modules.linear import Linear
-from torch.nn.modules.loss import MSELoss
 from torchvision import models
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
@@ -20,12 +15,16 @@ class RacingF1Detector(pl.LightningModule):
         super().__init__(*args, **kwargs)
 
         self.save_hyperparameters(hparams)
-        self.loss_function = MSELoss()
-        
+
         self.model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
         in_features = self.model.roi_heads.box_predictor.cls_score.in_features
         self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, self.OUT_VALUES)
 
+        # metrics to track
+        self.val_acc = torchmetrics.Accuracy()
+        self.test_acc = torchmetrics.Accuracy()
+        self.val_f1 = torchmetrics.F1()        
+        self.test_f1 = torchmetrics.F1()
 
 
     def forward(self, x: Tensor, **kwargs) -> Tensor:
@@ -34,8 +33,7 @@ class RacingF1Detector(pl.LightningModule):
 
         return out
 
-
-    def training_step(self, batch: dict, batch_idx: int) -> Tensor:
+    def step(self, batch: Dict[str, Tensor]) -> Dict[str, Tensor]:
 
         inputs = batch['img']
         labels = batch['bounding_box']
@@ -51,20 +49,79 @@ class RacingF1Detector(pl.LightningModule):
                 target_box = torch.FloatTensor(target_box).reshape(1, self.OUT_VALUES).to(self.device)
                 targets.append({ 'boxes': target_box, 'labels': target_labels })
                 inputs = torch.cat([inputs[0:i], inputs[i+1:]])
-        
-        loss_dict = self.model(inputs, targets)
-        loss = sum(loss for loss in loss_dict.values())
-        self.log("train_loss", loss, prog_bar=True, logger=True)
+
+        out_dict: Union[Dict[str, Tensor], List[Dict[str, Tensor]]] = self.model(inputs, targets)
+
+        if self.training:
+            # out_dict: Dict[str, Tensor]
+            loss = sum(loss for loss in out_dict.values())
+            return { 'loss': loss }
+        else:
+            # out_dict: List[Dict[str, Tensor]]
+            new_out_dict = dict()
+
+            for out_sample in out_dict:
+                for key, value in out_sample.items():
+
+                    tensor_value = torch.tensor([value])
+
+                    if key not in new_out_dict: new_out_dict[key] = tensor_value
+                    else: new_out_dict[key] = torch.cat((new_out_dict[key], tensor_value))
+
+            return new_out_dict
+
+
+    def training_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Tensor:
+
+        out = self.step(batch)
+
+        loss = out['loss']
+
+        self.log('train_loss', loss, prog_bar=True, logger=True)
 
         return loss
 
 
-    def validation_step(self, batch: dict, batch_idx: int) -> None:
-        raise NotImplementedError
+    def validation_step(self, batch: Dict[str, Tensor], batch_idx: int) -> None:
+
+        out = self.step(batch)
+
+        predictions = out['boxes']
+        labels = batch['labels']
+
+        accuracy = self.val_acc(predictions, labels)
+        f1_score = self.val_f1(predictions, labels)
+
+        self.log_dict(
+            {
+                'val_acc': accuracy,
+                'val_f1': f1_score,
+            },
+            prog_bar=True,
+            on_step=True,
+            on_epoch=True,
+        )
 
 
-    def test_step(self, batch: dict, batch_idx: int) -> Any:
-        raise NotImplementedError
+    def test_step(self, batch: Dict[str, Tensor], batch_idx: int) -> None:
+
+        out = self.step(batch)
+
+        predictions = out['boxes']
+        labels = batch['labels']
+
+        accuracy = self.test_acc(predictions, labels)
+        f1_score = self.test_f1(predictions, labels)
+
+        self.log_dict(
+            {
+                'test_acc': accuracy,
+                'test_f1': f1_score,
+            },
+            prog_bar=True,
+            on_step=True,
+            on_epoch=True,
+        )
 
 
     def training_epoch_end(self, outputs):
