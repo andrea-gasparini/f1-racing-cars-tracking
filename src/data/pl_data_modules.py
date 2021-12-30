87e345763e50968d6e9934e2475e4567d4219fb4
Andrea Gasparini <andrea.gasparini11@gmail.com>
2021-12-30 16:57:44 +0100

    Refactor DataModule
    Co-authored-by: Edoardo Di Paolo <aedoardo@users.noreply.github.com>

--- src/data/pl_data_modules.py
+++ src/data/pl_data_modules.py
@@ -1,4 +1,7 @@
-from typing import List, Optional, Union
+import os
+import torch
+
+from typing import Generator, List, Optional, Union
 from torch.utils.data import DataLoader
 from src.data.datasets import RacingF1Dataset
 from torchvision.transforms import Compose
@@ -8,45 +11,42 @@ import pytorch_lightning as pl
 
 class RacingF1DataModule(pl.LightningDataModule):
 
-    def __init__(self, batch_size: int, dataset_dir: str,
-                       val_subdirs: List[str]   = ["racing-10", "racing-11"],
-                       train_subdirs: List[str] = ["racing-1", "racing-7",
-                                                   "racing-9", "racing-20"],
-                       test_subdirs: List[str]  = ["racing-12"],
-                       data_transform: Optional[Compose] = None):
+    def __init__(self, batch_size: int, dataset: RacingF1Dataset) -> None:
 
         super().__init__()
+
         self.batch_size = batch_size
-        self.dataset_dir = dataset_dir
-        self.train_dirs = join_dirs(self.dataset_dir, train_subdirs)
-        self.val_dirs = join_dirs(self.dataset_dir, val_subdirs)
-        self.test_dirs = join_dirs(self.dataset_dir, test_subdirs)
-        self.data_transform = data_transform
+        self.dataset = dataset
 
-    # def prepare_data(self, *args, **kwargs):
-    # 	raise NotImplementedError
+    @staticmethod
+    def from_directories(batch_size: int, dataset_root_dir: str, subdirs: List[str] = None,
+                         data_transform: Optional[Compose] = None) -> "RacingF1DataModule":
 
+        if subdirs is None:
+            subdirs = os.listdir(dataset_root_dir)
+        
+        datasets_dirs = join_dirs(dataset_root_dir, subdirs)
 
-    def setup(self, stage: Optional[str] = None) -> None:
+        return RacingF1DataModule(batch_size, RacingF1Dataset(datasets_dirs, data_transform))
+
+    def setup(self, stage: Optional[str] = None, train_size: float = 0.6,
+              val_size: float = 0.25, test_size: float = 0.15,
+              rnd_generator: Generator = torch.Generator().manual_seed(42)) -> None:
+
+        trainset, testset, valset = self.dataset.random_split(train_size, val_size, test_size, rnd_generator)
+
         if stage == 'fit' or stage is None:
-            self.train_dataset = RacingF1Dataset(self.train_dirs, self.data_transform)
-            self.val_dataset = RacingF1Dataset(self.val_dirs, self.data_transform)
+            self.train_dataset = trainset
+            self.val_dataset = valset
         
         if stage == 'test' or stage is None:
-            self.test_set = RacingF1Dataset(self.test_dirs, self.data_transform)
-
+            self.test_dataset = testset
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self.train_dataset, batch_size=self.batch_size)
 
-
     def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
         return DataLoader(self.val_dataset, batch_size=self.batch_size)
 
-
     def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
-        return DataLoader(self.test_set, batch_size=self.batch_size)
-
-
-    # def transfer_batch_to_device(self, batch: Any, device: torch.device) -> Any:
-    # 	raise NotImplementedError
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
